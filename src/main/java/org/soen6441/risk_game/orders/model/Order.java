a0170aea2516bd5d63131e961f2c06f7ef12615f
Ahmed Fakhir <ahmed.simo.a@hotmail.fr>
2025-02-21 13:48:40 -0500

    Add Order related entities

---
+++ src/main/java/org/soen6441/risk_game/orders/model/Order.java
@@ -0,0 +1,16 @@
+package org.soen6441.risk_game.orders.model;
+
+import org.soen6441.risk_game.game_engine.model.GameSession;
+
+/**
+ * This interface represents the generic Order that players
+ * can instruct.
+ */
+public interface Order {
+    /**
+     * This class executes the order by updating
+     * accordingly the provided gameSession.
+     * @param p_gameSession The game session
+     */
+    void execute(GameSession p_gameSession);
+}
