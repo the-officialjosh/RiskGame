bd04e5f6798a00d39af3457d413f06a2e1580e14
Ahmed Fakhir <ahmed.simo.a@hotmail.fr>
2025-02-21 13:48:23 -0500

    Add player related controller and models

---
+++ src/main/java/org/soen6441/risk_game/player_management/controller/PlayerController.java
@@ -0,0 +1,16 @@
+package org.soen6441.risk_game.player_management.controller;
+
+import org.soen6441.risk_game.game_engine.model.GameSession;
+
+/**
+ * This class holds all the logic for players management.
+ */
+public class PlayerController {
+    /**
+     * This method handles the players loading step.
+     * @param p_gameSession The game session.
+     */
+    public void loadPlayers(GameSession p_gameSession) {
+
+    }
+}
