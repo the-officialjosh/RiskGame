856e5645498d7c58d7cbf7b05b1a9777167c8a10
irfan <irfan.nizar@unikrew.com>
2025-02-24 14:25:08 -0500

    Load players in a particular game session.

--- src/main/java/org/soen6441/risk_game/player_management/controller/PlayerController.java
+++ src/main/java/org/soen6441/risk_game/player_management/controller/PlayerController.java
@@ -1,6 +1,12 @@
 package org.soen6441.risk_game.player_management.controller;
 
 import org.soen6441.risk_game.game_engine.model.GameSession;
+import org.soen6441.risk_game.orders.model.Order;
+import org.soen6441.risk_game.player_management.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * This class holds all the logic for players management.
@@ -11,6 +17,31 @@ public class PlayerController {
      * @param p_gameSession The game session.
      */
     public void loadPlayers(GameSession p_gameSession) {
+        Scanner l_scanner = new Scanner(System.in);
+        String l_addPlayerCommand = l_scanner.nextLine();
+        String[] l_addPlayerCommandArray = l_addPlayerCommand.split(" ");
+        String l_command = l_addPlayerCommandArray[0];
+        String l_action = l_addPlayerCommandArray[1];
+        List<Player> playerList = new ArrayList<>();
+        if(!l_command.equals("gameplayer")){
+            System.out.println("Invalid Command");
+            return;
+        }
+        if(!l_action.equals("-add") && !l_action.equals("-remove")){
+            System.out.println("Invalid Command");
+            return;
+        }
+        if(l_action.equals("-add")){
+            for(int i= 2; i < l_addPlayerCommandArray.length; i++){
+                Player player = new Player(l_addPlayerCommandArray[i],0,new ArrayList<Order>());
+                playerList.add(player);
+            }
+        }
+//        for(int i = 0; i < playerList.size(); i++){
+//            System.out.println(playerList.get(i).getName());
+//        }
+        p_gameSession.setPlayers(playerList);
+
 
     }
 }
