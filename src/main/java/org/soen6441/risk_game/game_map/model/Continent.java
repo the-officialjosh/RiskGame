abdafb67762bfe661fc931bb8a1cdaf315f5e399
KawshikKumar <kawshik.gsh@gmail.com>
2025-02-27 17:44:11 -0500

    Added methods

--- src/main/java/org/soen6441/risk_game/game_map/model/Continent.java
+++ src/main/java/org/soen6441/risk_game/game_map/model/Continent.java
@@ -8,15 +8,17 @@ import java.util.List;
 public class Continent {
     private String d_name;
     private List<Country> d_countries;
-
+    private int d_value;
     /**
      * Contructor for the class.
      * @param p_name The name of the continent.
      * @param p_countries The countries that will be inside the continent.
+     * @param p_value The value of the continent.
      */
-    public Continent(String p_name, List<Country> p_countries) {
+    public Continent(String p_name, List<Country> p_countries, int p_value) {
         this.d_name = p_name;
         this.d_countries = p_countries;
+        this.d_value = p_value;
     }
 
     /**
@@ -49,5 +51,36 @@ public class Continent {
      */
     public void setCountries(List<Country> p_territories) {
         this.d_countries = p_territories;
+    }
+    /**
+     * Returns the value of the continent.
+     * @return The value of the continent.
+     */
+    public int getValue() {
+        return d_value;
+    }
+
+    /**
+     * Sets the value of the continent.
+     * @param p_value The value of the continent.
+     */
+    public void setValue(int p_value) {
+        this.d_value = p_value;
+    }
+
+    /**
+     * Adds a country to the continent.
+     * @param country The country to add.
+     */
+    public void addCountry(Country country) {
+        d_countries.add(country);
+    }
+
+    /**
+     * Removes a country from the continent.
+     * @param countryName The name of the country to remove.
+     */
+    public void removeCountry(String countryName) {
+        d_countries.removeIf(country -> country.getName().equals(countryName));
     }
 }
