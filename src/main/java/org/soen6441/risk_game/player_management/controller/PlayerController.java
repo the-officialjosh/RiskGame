aff995f66ff7edd1d36cb12b1046e7e3ce21da69
Joshua Onyema <onyemajoshua892@gmail.com>
2025-03-05 01:01:40 -0500

    code cleanup

--- src/main/java/org/soen6441/risk_game/player_management/controller/PlayerController.java
+++ src/main/java/org/soen6441/risk_game/player_management/controller/PlayerController.java
@@ -24,51 +24,64 @@ public class PlayerController {
      * @param p_gameSession The game session.
      */
     public void loadPlayers(GameSession p_gameSession) {
-        d_displayToUser.instructionMessage("Add players to the game by using (\"gameplayer -add nameofplayer\") command.\nAfter adding players you can use (\"assigncountries\")command");
+        d_displayToUser.instructionMessage("\n⚔ PLAYER MANAGEMENT STEP");
+        d_displayToUser.instructionMessage("----------------------------------------");
+        d_displayToUser.instructionMessage("Use the following commands to manage players:");
+        d_displayToUser.instructionMessage("- ⚒ gameplayer -add <playername>  (Add a new player)");
+        d_displayToUser.instructionMessage("- ⚒ gameplayer -remove <playername>  (Remove a player)");
+        d_displayToUser.instructionMessage("- ⚒ assigncountries  (Assign countries to players)");
+        d_displayToUser.instructionMessage("----------------------------------------\n");
+
         Scanner l_scanner = new Scanner(System.in);
-        String l_command = "";
-        String l_addPlayerCommand;
+        String l_command;
         List<Player> playerList = new ArrayList<>();
+
         do {
-            l_addPlayerCommand = l_scanner.nextLine();
-            String[] l_addPlayerCommandArray = l_addPlayerCommand.split(" ");
-            l_command = l_addPlayerCommandArray[0];
+            l_command = l_scanner.nextLine();
+            String[] l_commandArray = l_command.split(" ");
+
             if (!playerList.isEmpty() && l_command.equals("assigncountries")) {
                 break;
             }
 
-            if (!l_command.equals("gameplayer")) {
-                d_displayToUser.instructionMessage("Invalid Command (To add player use this command \"gameplayer -add nameofplayer\")");
+            if (!l_commandArray[0].equals("gameplayer")) {
+                d_displayToUser.instructionMessage("⚠ Invalid Command! Use: gameplayer -add <playername> or gameplayer -remove <playername>");
                 continue;
             }
-            String l_action = l_addPlayerCommandArray[1];
-            if (!l_action.equals("-add") && !l_action.equals("-remove")) {
-                d_displayToUser.instructionMessage("Invalid Command (To add player use this command \"gameplayer -add nameofplayer\"");
+
+            if (l_commandArray.length < 3) {
+                d_displayToUser.instructionMessage("⚠ Invalid Syntax! Correct usage: gameplayer -add <playername>");
                 continue;
             }
+
+            String l_action = l_commandArray[1];
+            String l_playerName = l_commandArray[2];
+
             if (l_action.equals("-add")) {
-                Player playerToAdd = new Player(l_addPlayerCommandArray[2], 0, new ArrayList<Order>());
+                Player playerToAdd = new Player(l_playerName, 0, new ArrayList<Order>());
                 boolean exists = playerList.stream().anyMatch(player -> player.getName().equalsIgnoreCase(playerToAdd.getName()));
                 if (exists) {
-                    d_displayToUser.instructionMessage("Player already exist. Try with different name");
+                    d_displayToUser.instructionMessage("⚠ Player already exists. Try a different name.");
                     continue;
                 }
                 playerList.add(playerToAdd);
-                d_displayToUser.instructionMessage("Player added.");
-            }
-            if (l_action.equals("-remove")) {
+                d_displayToUser.instructionMessage("✔ Player " + l_playerName + " added.");
+            } else if (l_action.equals("-remove")) {
                 if (!playerList.isEmpty()) {
-                    boolean isRemoved = playerList.removeIf(player -> player.getName().equalsIgnoreCase(l_addPlayerCommandArray[2]));
+                    boolean isRemoved = playerList.removeIf(player -> player.getName().equalsIgnoreCase(l_playerName));
                     if (isRemoved) {
-                        d_displayToUser.instructionMessage("Player removed");
+                        d_displayToUser.instructionMessage("✔ Player " + l_playerName + " removed.");
                     } else {
-                        d_displayToUser.instructionMessage("Could not found player with this name");
+                        d_displayToUser.instructionMessage("⚠ Player not found.");
                     }
                 } else {
-                    d_displayToUser.instructionMessage("There are no players to remove.");
+                    d_displayToUser.instructionMessage("⚠ No players to remove.");
                 }
+            } else {
+                d_displayToUser.instructionMessage("⚠ Invalid action. Use -add or -remove.");
             }
         } while (true);
+
         p_gameSession.setPlayers(playerList);
     }
 
@@ -78,9 +91,9 @@ public class PlayerController {
             allArmiesDeployed = true;
             for (Player player : p_gameSession.getPlayers()) {
                 d_gameMapController.showMap(p_gameSession.getMap());
-                d_displayToUser.instructionMessage("\nReinforcement Armies");
-                d_displayToUser.instructionMessage("=====================");
-                d_displayToUser.instructionMessage(player.getName() + " you have (" + player.getNumberOfReinforcementsArmies() + ") reinforcement armies");
+                d_displayToUser.instructionMessage("\n⚔ Reinforcement Phase");
+                d_displayToUser.instructionMessage("==========================");
+                d_displayToUser.instructionMessage(player.getName() + " you have (" + player.getNumberOfReinforcementsArmies() + ") reinforcement armies.");
 
                 if (player.hasReinforcementsArmies()) {
                     player.issue_order();
@@ -90,12 +103,12 @@ public class PlayerController {
                 }
             }
         } while (!allArmiesDeployed);
-        d_displayToUser.instructionMessage("All armies has been deployed");
+        d_displayToUser.instructionMessage("✔ All armies have been deployed.");
     }
 
     public void executeOrder(GameSession p_gameSession) {
-        for (int i = 0; i < p_gameSession.getPlayers().size(); i++) {
-            p_gameSession.getPlayers().get(i).next_order();
+        for (Player player : p_gameSession.getPlayers()) {
+            player.next_order();
         }
     }
 }
