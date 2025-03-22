19034b3a585f6ff902f7875374b6ff9ce77a7243
Safin Mahesania <safin.n.mahesania@gmail.com>
2025-03-22 17:17:04 -0400

    Bomb Card functionality added

---
+++ src/main/java/org/soen6441/risk_game/orders/model/Bomb.java
@@ -0,0 +1,30 @@
+package org.soen6441.risk_game.orders.model;
+
+import org.soen6441.risk_game.game_engine.model.GameSession;
+import org.soen6441.risk_game.game_map.model.Country;
+import org.soen6441.risk_game.player_management.model.Player;
+
+import java.util.HashMap;
+
+public class Bomb implements Order {
+
+    Player d_player;
+    Country d_sourceCountry;
+    Country d_targetCountry;
+
+    public Bomb(Country p_sourceCountry, Player p_player, Country p_targetCountry) {
+        this.d_sourceCountry = p_sourceCountry;
+        this.d_player = p_player;
+        this.d_targetCountry = p_targetCountry;
+    }
+
+    @Override
+    public void execute() {
+        GameSession l_gameSession = GameSession.getInstance();
+        Player targetCountryOwnedBy = l_gameSession.getMap().getCountriesById(d_targetCountry.getCountryId()).getD_ownedBy();
+        int l_armies = (l_gameSession.getMap().getCountriesById(d_targetCountry.getCountryId()).getExistingArmies().get(targetCountryOwnedBy)) / 2;
+        HashMap<Player, Integer> map = new HashMap<>();
+        map.put(targetCountryOwnedBy, l_armies);
+        l_gameSession.getMap().getCountriesById(d_targetCountry.getCountryId()).setExistingArmies(map);
+    }
+}
