eedecb31c1438187246cdc7cda26cc1892d7d9c1
Safin Mahesania <safin.n.mahesania@gmail.com>
2025-03-22 17:21:42 -0400

    Bomb Card Java Doc Updated

--- src/main/java/org/soen6441/risk_game/orders/model/Bomb.java
+++ src/main/java/org/soen6441/risk_game/orders/model/Bomb.java
@@ -3,15 +3,33 @@ package org.soen6441.risk_game.orders.model;
 import org.soen6441.risk_game.game_engine.model.GameSession;
 import org.soen6441.risk_game.game_map.model.Country;
 import org.soen6441.risk_game.player_management.model.Player;
-
 import java.util.HashMap;
 
+/**
+ * The type Bomb.
+ */
 public class Bomb implements Order {
 
+    /**
+     * The D player.
+     */
     Player d_player;
+    /**
+     * The D source country.
+     */
     Country d_sourceCountry;
+    /**
+     * The D target country.
+     */
     Country d_targetCountry;
 
+    /**
+     * Instantiates a new Bomb.
+     *
+     * @param p_sourceCountry the p source country
+     * @param p_player        the p player
+     * @param p_targetCountry the p target country
+     */
     public Bomb(Country p_sourceCountry, Player p_player, Country p_targetCountry) {
         this.d_sourceCountry = p_sourceCountry;
         this.d_player = p_player;
