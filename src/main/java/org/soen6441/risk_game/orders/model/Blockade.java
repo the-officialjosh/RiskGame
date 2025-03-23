69aa3a644271083cb8f7ac9a1e281a48a7d5ce2d
Safin Mahesania <safin.n.mahesania@gmail.com>
2025-03-23 13:49:04 -0400

    Blockade Card Functionality Added

---
+++ src/main/java/org/soen6441/risk_game/orders/model/Blockade.java
@@ -0,0 +1,26 @@
+package org.soen6441.risk_game.orders.model;
+
+import org.soen6441.risk_game.game_engine.model.GameSession;
+import org.soen6441.risk_game.game_map.model.Country;
+import org.soen6441.risk_game.player_management.model.Player;
+import java.util.HashMap;
+
+public class Blockade implements Order {
+    private Player l_player;
+    private Country l_country;
+
+    public Blockade(Player p_player, Country p_country) {
+        this.l_player = p_player;
+        this.l_country = p_country;
+    }
+
+
+    @Override
+    public void execute() {
+        GameSession l_gameSession = GameSession.getInstance();
+        int l_currentArmies = l_gameSession.getMap().getCountriesById(l_country.getCountryId()).getExistingArmies().get(l_player);
+        HashMap<Player, Integer> map = new HashMap<>();
+        map.put(l_player, (l_currentArmies * 3));
+        l_gameSession.getMap().getCountriesById(l_country.getCountryId()).setExistingArmies(map);
+    }
+}
