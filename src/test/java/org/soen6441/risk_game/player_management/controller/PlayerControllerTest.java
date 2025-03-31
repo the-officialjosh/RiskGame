59e65d1530189eac9c7a819120d0e1bded351664
Ahmed Fakhir <ahmed.simo.a@hotmail.fr>
2025-03-30 20:26:08 -0400

    Refactor PlayerControllerTest to use the new GameSession object access

--- src/test/java/org/soen6441/risk_game/player_management/controller/PlayerControllerTest.java
+++ src/test/java/org/soen6441/risk_game/player_management/controller/PlayerControllerTest.java
@@ -1,5 +1,6 @@
 package org.soen6441.risk_game.player_management.controller;
 import org.junit.jupiter.api.Test;
+import org.soen6441.risk_game.game_engine.model.GameSession;
 import org.soen6441.risk_game.orders.model.Order;
 import org.soen6441.risk_game.player_management.model.Player;
 
@@ -12,21 +13,21 @@ class PlayerControllerTest {
 
     @Test
     public void testDeployArmyMoreThenReinforcementPool(){
-        Player player = new Player("Player1",3,new ArrayList<Order>());
+        Player player = new Player("Player1",3,new ArrayList<Order>(), new GameSession());
         boolean result = player.validNumberOfReinforcementArmies(5);
         assertEquals(false,result);
     }
 
     @Test
     public void testDeployArmyEqualToReinforcementPool(){
-        Player player = new Player("Player1",3,new ArrayList<Order>());
+        Player player = new Player("Player1",3,new ArrayList<Order>(), new GameSession());
         boolean result = player.validNumberOfReinforcementArmies(3);
         assertEquals(true,result);
     }
 
     @Test
     public void testDeployArmyLessThenReinforcementPool(){
-        Player player = new Player("Player1",3,new ArrayList<Order>());
+        Player player = new Player("Player1",3,new ArrayList<Order>(), new GameSession());
         boolean result = player.validNumberOfReinforcementArmies(2);
         assertEquals(true,result);
     }
