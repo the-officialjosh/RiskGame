5b040010f326b75cca8760c35b9d1903a99bed5b
Ahmed Fakhir <ahmed.simo.a@hotmail.fr>
2025-03-30 20:20:22 -0400

    Add Tournament mode to GameEngine

--- src/main/java/org/soen6441/risk_game/game_engine/controller/GameEngine.java
+++ src/main/java/org/soen6441/risk_game/game_engine/controller/GameEngine.java
@@ -1,9 +1,6 @@
 package org.soen6441.risk_game.game_engine.controller;
 
-import org.soen6441.risk_game.game_engine.controller.state.ExecuteOrderPhase;
-import org.soen6441.risk_game.game_engine.controller.state.IssueOrderPhase;
-import org.soen6441.risk_game.game_engine.controller.state.Phase;
-import org.soen6441.risk_game.game_engine.controller.state.StartupPhase;
+import org.soen6441.risk_game.game_engine.controller.state.*;
 import org.soen6441.risk_game.game_engine.model.GameSession;
 import org.soen6441.risk_game.game_map.view.DisplayToUser;
 import org.soen6441.risk_game.monitoring.LogEntryBuffer;
@@ -23,13 +20,11 @@ public class GameEngine {
     private static String outputFolder = "out/";
 
     /**
-     * Main method which is the entry point method of the game.
-     *
-     * @param args The command line arguments.
+     * Main method which is the entry point method of the game
      */
     public static void main(String[] args) {
         // Game Session
-        GameSession l_gameSession = GameSession.getInstance();
+        GameSession l_gameSession = new GameSession();
         // Game Phase
         Phase phase;
 
@@ -38,6 +33,7 @@ public class GameEngine {
 
         // Configure Observer pattern for monitoring
         LogEntryBuffer.getInstance().addObserver(new LogEntryBufferObserver(outputFolder + "observed_actions.txt"));
+        LogEntryBuffer.getInstance().setValue("Single game mode chosen.");
 
         l_displayToUser.welcomeMessage();
 
@@ -54,6 +50,26 @@ public class GameEngine {
             // Execute order phase
             phase = new ExecuteOrderPhase();
             phase.handlePhase(l_gameSession);
+        }
+    }
+
+    public static void startTournamentModeGame(String l_tournamentCommand) {
+        // Game Phase
+        Phase phase;
+
+        // Configure Observer pattern for monitoring
+        LogEntryBuffer.getInstance().addObserver(new LogEntryBufferObserver(outputFolder + "observed_actions.txt"));
+        LogEntryBuffer.getInstance().setValue("Tournament mode chosen.");
+
+        // Startup Phase for Tournament games
+        phase = new TournamentPhase(l_tournamentCommand);
+
+        try {
+            phase.handlePhase(new GameSession());
+        } catch (Exception e) {
+            System.out.print("Unexpected Error in Tournament Gameplay:\n");
+            e.printStackTrace();
+            System.exit(0);
         }
     }
 }
