d57a34bc8e91642717b93ee340e82891f224cb94
irfan <irfan.nizar@unikrew.com>
2025-04-03 21:29:30 -0400

    Added constructors

--- 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/AggressivePlayer.java
+++ 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/AggressivePlayer.java
@@ -1,8 +1,23 @@
 package org.soen6441.risk_game.player_management.model;
 
+import org.soen6441.risk_game.game_engine.model.GameSession;
+import org.soen6441.risk_game.game_map.view.DisplayToUser;
 import org.soen6441.risk_game.player_management.strategy.PlayerStrategy;
 
-public class AggressivePlayer implements PlayerStrategy {
+import java.io.Serializable;
+
+public class AggressivePlayer implements PlayerStrategy, Serializable {
+
+    private final Player d_player;
+    private DisplayToUser d_displayToUser;
+    private GameSession d_gameSession;
+
+    public AggressivePlayer(Player d_player, GameSession d_gameSession) {
+        this.d_player = d_player;
+        this.d_displayToUser = d_displayToUser;
+        this.d_gameSession = d_gameSession;
+    }
+
     @Override
     public void issueOrder() {
 
--- 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/RandomPlayer.java
+++ 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/RandomPlayer.java
@@ -1,8 +1,22 @@
 package org.soen6441.risk_game.player_management.model;
 
+import org.soen6441.risk_game.game_engine.model.GameSession;
+import org.soen6441.risk_game.game_map.view.DisplayToUser;
 import org.soen6441.risk_game.player_management.strategy.PlayerStrategy;
 
-public class RandomPlayer implements PlayerStrategy {
+import java.io.Serializable;
+
+public class RandomPlayer implements PlayerStrategy, Serializable {
+    private final Player d_player;
+    private DisplayToUser d_displayToUser;
+    private GameSession d_gameSession;
+
+    public RandomPlayer(Player d_player, GameSession d_gameSession) {
+        this.d_player = d_player;
+        this.d_gameSession = d_gameSession;
+        this.d_displayToUser = new DisplayToUser();
+    }
+
     @Override
     public void issueOrder() {
 
