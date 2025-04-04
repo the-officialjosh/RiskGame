55433051c4b9dff0884ae79f52c4e28e91860b0c
KawshikKumar <kawshik.gsh@gmail.com>
2025-04-04 15:10:16 -0400

    Enhance RandomPlayer strategy with random deployment and advance order logic

--- 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/RandomPlayer.java
+++ 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/RandomPlayer.java
@@ -1,24 +1,48 @@
 package org.soen6441.risk_game.player_management.model;
 
 import org.soen6441.risk_game.game_engine.model.GameSession;
-import org.soen6441.risk_game.game_map.view.DisplayToUser;
+import org.soen6441.risk_game.game_map.model.Country;
+import org.soen6441.risk_game.orders.model.Advance;
+import org.soen6441.risk_game.orders.model.Deploy;
 import org.soen6441.risk_game.player_management.strategy.PlayerStrategy;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
 
 public class RandomPlayer implements PlayerStrategy, Serializable {
     private final Player d_player;
-    private DisplayToUser d_displayToUser;
     private GameSession d_gameSession;
 
     public RandomPlayer(Player d_player, GameSession d_gameSession) {
         this.d_player = d_player;
         this.d_gameSession = d_gameSession;
-        this.d_displayToUser = new DisplayToUser();
     }
 
     @Override
     public void issueOrder() {
+        Random rand = new Random();
+        List<Country> owned = d_player.getD_countries_owned();
+        if (owned.isEmpty()) return;
 
+        // Deploy randomly
+        if (d_player.getNumberOfReinforcementsArmies() > 0) {
+            Country randomCountry = owned.get(rand.nextInt(owned.size()));
+            Deploy deployOrder = new Deploy(d_player, d_player.getNumberOfReinforcementsArmies(), randomCountry.getCountryId());
+            deployOrder.setD_gameSession(d_gameSession);
+            d_player.setOrders(deployOrder);
+            d_player.setNumberOfReinforcementsArmies(0);
+        }
+
+        // Random advance order
+        Country from = owned.get(rand.nextInt(owned.size()));
+        List<Country> adjacent = from.getAdjacentCountries();
+        if (!adjacent.isEmpty()) {
+            Country to = adjacent.get(rand.nextInt(adjacent.size()));
+            int armies = rand.nextInt(from.getExistingArmies()) + 1;
+            Advance advanceOrder = new Advance(d_player, from, to, armies);
+            advanceOrder.setD_gameSession(d_gameSession);
+            d_player.setOrders(advanceOrder);
+        }
     }
 }
