2efd1b839ead4de2a16d94a92a013639f89bd66d
KawshikKumar <kawshik.gsh@gmail.com>
2025-04-04 15:10:27 -0400

    Enhance CheaterPlayer strategy with conquering adjacent enemy countries and doubling armies on enemy borders

--- 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/CheaterPlayer.java
+++ 3rd Delivery/src/main/java/org/soen6441/risk_game/player_management/model/CheaterPlayer.java
@@ -1,24 +1,48 @@
 package org.soen6441.risk_game.player_management.model;
 
 import org.soen6441.risk_game.game_engine.model.GameSession;
-import org.soen6441.risk_game.game_map.view.DisplayToUser;
+import org.soen6441.risk_game.game_map.model.Country;
 import org.soen6441.risk_game.player_management.strategy.PlayerStrategy;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class CheaterPlayer implements PlayerStrategy, Serializable {
     private final Player d_player;
-    private DisplayToUser d_displayToUser;
     private GameSession d_gameSession;
 
     public CheaterPlayer(Player d_player, GameSession d_gameSession) {
         this.d_player = d_player;
         this.d_gameSession = d_gameSession;
-        this.d_displayToUser = new DisplayToUser();
     }
 
     @Override
     public void issueOrder() {
+        // Conquer all adjacent enemy countries
+        for (Country country : new ArrayList<>(d_player.getD_countries_owned())) {
+            for (Country neighbor : country.getAdjacentCountries()) {
+                if (neighbor.getD_ownedBy() != null && !neighbor.getD_ownedBy().equals(d_player)) {
+                    // Transfer ownership
+                    neighbor.getD_ownedBy().getD_countries_owned().remove(neighbor);
+                    neighbor.setD_ownedBy(d_player);
+                    d_player.getD_countries_owned().add(neighbor);
+                    neighbor.setExistingArmies(1);
+                }
+            }
+        }
 
+        // Double armies on countries with enemy neighbors
+        for (Country country : d_player.getD_countries_owned()) {
+            boolean hasEnemy = false;
+            for (Country neighbor : country.getAdjacentCountries()) {
+                if (neighbor.getD_ownedBy() != null && !neighbor.getD_ownedBy().equals(d_player)) {
+                    hasEnemy = true;
+                    break;
+                }
+            }
+            if (hasEnemy) {
+                country.setExistingArmies(country.getExistingArmies() * 2);
+            }
+        }
     }
 }
